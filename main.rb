e29db640ec2d2e27c466a0a52214a2b5887b547a
Matthew Bunday <zenceph@gmail.com>
2013-10-05 20:30:51 -0400

    Add gemfile and main.rb

---
+++ main.rb
@@ -0,0 +1,39 @@
+require 'rubygems'
+require 'bundler/setup'
+
+require 'sinatra'
+require 'mongo'
+
+class Alphabet < Sinatra::Base
+    enable :sessions
+    set :bind, '0.0.0.0'
+    set :port, 1111
+
+    helpers do
+        def logged_in?
+            !session[:user].nil?
+        end
+
+        def user
+            session[:user]
+        end
+    end
+
+    set(:auth) do |roles|
+        condition do
+            redirect '/', 303 unless logged_in?
+        end
+    end
+
+    get '/' do
+        liquid :index
+    end
+
+    get '/login' do
+        liquid :login
+    end
+
+    get '/logout' do
+
+    end
+end
