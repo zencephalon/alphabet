c7fd168012ed3073265e2e323eb16422528205ca
Matthew Bunday <zenceph@gmail.com>
2013-10-06 05:25:47 -0400

    Fix bug

--- alphabet/alphabet.rb
+++ alphabet/alphabet.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'mongo'
 require_relative 'bet'
+require_relative 'user'
 
 class Alphabet
     attr_reader :db, :bet_m
@@ -9,6 +10,6 @@ class Alphabet
         @client = client
         @db = @client.db('alphabet')
         @bet_m = Bets::BetManager.new(self)
-        @user_m = Bets::UserManager.new(self)
+        @user_m = Users::UserManager.new(self)
     end
 end
